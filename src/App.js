e1abe8e94e558f68e5eaf21b3c6c9e3e12d49828
jang294 <tjrwjsjje@naver.com>
2023-09-21 09:08:02 +0900

    commit first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> 안녕 세상아 ~~ </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
